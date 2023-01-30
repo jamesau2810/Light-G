1b5c8f76ac21b728637b649b84fd53193f455d0e
James <pmau@connect.ust.hk>
2023-01-30 12:05:44 +0800

    Initial Commit

---
+++ Light-G/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Light-G
+//
+//  Created by James Au on 30/1/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
